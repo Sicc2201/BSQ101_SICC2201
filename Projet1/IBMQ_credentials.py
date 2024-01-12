1348c79c10d378207d7f49380306583f73832f00
Christopher <kikiller_gamer@hotmail.com>
2024-01-12 09:11:31 -0500

    added IBMQ credentials

---
+++ Projet1/IBMQ_credentials.py
@@ -0,0 +1,40 @@
+
+##########################################################################
+
+# Titre: IBMQ_credentials.py
+# Author: Christopher Sicotte (SICC2201)
+# last modified: 10/01/2024
+
+##########################################################################
+'''
+# Description: 
+'''
+
+###########################################################################
+
+# IMPORTS
+
+###########################################################################
+
+# importing Qiskit
+from qiskit_ibm_runtime import QiskitRuntimeService
+
+
+###########################################################################
+
+# Utility
+
+###########################################################################
+
+
+def ibmq_connexion(ID):
+    # Save an IBM Quantum account and set it as your default account.
+    QiskitRuntimeService.save_account(channel="ibm_quantum", token=ID, set_as_default=True)
+
+def ibmq_provider(provider_name):
+
+    provider = QiskitRuntimeService()
+    provider.backends()  # list of backends
+    backend = provider.backend(provider_name)
+
+    print(backend.configuration().basis_gates)
--- Projet1/main.py
+++ Projet1/main.py
@@ -15,10 +15,37 @@
 
 ###########################################################################
 
+# Custom libraries
+import IBMQ_credentials
 
-
+# import packages
+from sympy import *
 ###########################################################################
 
 # MAIN
 
 ###########################################################################
+
+
+def main():
+
+    ibmq_token = ""  #put your IBMQ account Token here
+
+    ####  uncomment if this is the first time you connect to your account  ##########
+    # IBMQ_credentials.ibmq_connexion(ibmq_token)
+    #################################################################################
+
+    IBMQ_credentials.ibmq_provider("ibmq_qasm_simulator")
+    
+    Xa, Xb, Xc, Xd, Xe = symbols('Xa, Xb, Xc, Xd, Xe')
+    Pa = (~Xe & ~Xb) | (Xe & Xb)
+    Pb = (~Xc & ~Xe) | (Xc & Xe)
+    Pc = (~Xe & ~Xa) | (Xe & Xa)
+    Pd = (~Xc & ~Xb) | (Xc & Xb)
+    Pe = (~Xd & ~Xa) | (Xd & Xa)
+
+    Pg = Pa & Pb & Pc & Pd & Pe
+    return Pa
+
+if __name__ == "__main__":
+    main()
