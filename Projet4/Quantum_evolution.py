4f6e19ec24727748055e246c42ac17848f41a124
Sicc2201 <107315096+Sicc2201@users.noreply.github.com>
2024-04-03 11:35:58 -0400

    project start

---
+++ Projet4/Quantum_evolution.py
@@ -0,0 +1,175 @@
+
+##########################################################################
+
+# Titre: Quantum_chemistry.py
+# Author: Christopher Sicotte (SICC2201)
+# last modified: 26/03/2024
+
+##########################################################################
+'''
+# Description: 
+
+Ce fichier contient toutes fonctions qui gèrent le processus d'évolution d'un système quantique.
+
+'''
+
+###########################################################################
+
+# IMPORTS
+
+###########################################################################
+from qiskit import QuantumCircuit
+from qiskit.circuit import Parameter
+from qiskit.providers.backend import Backend
+from qiskit.quantum_info import Pauli, PauliList, SparsePauliOp
+import numpy as np
+from numpy.typing import NDArray, ArrayLike
+from typing import List, Callable, Union
+from scipy.optimize import OptimizeResult, minimize
+
+#custom library
+import Utils
+import Pauli_operations as po
+
+###########################################################################
+
+# METHODS
+
+###########################################################################
+
+def exact_evolution(
+initial_state: QuantumCircuit,
+hamiltonian: SparsePauliOp,
+time_values: NDArray[np.float_],
+observables: List[SparsePauliOp],
+):
+    """
+    Simulate the exact evolution of a quantum system in state initial_state under a given
+    hamiltonian for different time_values. The result is a series of expected values
+    for given observables.
+    Args:
+    time_values (NDArray[np.float_]): An array of time values.
+    initial_state (QuantumCircuit): The circuit preparing the initial quantum state.
+    hamiltonian (SparsePauliOp): The Hamiltonien of the system
+    observables (List[SparsePauliOp]): The observable to be measure at each the the
+    time_values.
+
+    Returns:
+    NDArray[np.float_]: The expected values of the observable. Should be of shape
+    (len(time_values), len(observables)).
+    """
+
+    observables_expected_values = np.empty(len(time_values), len(observables))
+
+    energy, base_matrix = diagonalize_hamiltonian(hamiltonian)
+
+    w = time_values[:, None] * energy[:, None]
+
+
+
+
+    return observables_expected_values
+
+
+def diagonalize_hamiltonian(hamiltonian: SparsePauliOp):
+    eigenvalues, eigenvector = np.linalg.eigh(hamiltonian.to_matrix())
+    return eigenvalues, eigenvector
+
+
+
+
+
+
+
+def trotter_evolution(
+initial_state: QuantumCircuit,
+hamiltonian: SparsePauliOp,
+time_values: NDArray[np.float_],
+observables: List[SparsePauliOp],
+num_trotter_steps: NDArray[np.int_],
+):
+    """
+    Simulate, using Trotterisation, the evolution of a quantum system in state initial_state
+    under a given hamiltonian for different time_values. The result is a series of
+    expected values for given observables.
+
+    Args:
+    time_values (NDArray[np.float_]): An array of time values.
+    initial_state (QuantumCircuit): The circuit preparing the initial quantum state.
+    hamiltonian (SparsePauliOp): The Hamiltonien of the system
+    observables (List[SparsePauliOp]): The observable to be measure at each the the
+    time_values.
+    num_trotter_steps: (NDArray[np.int_]): The number of steps of the Trotterization for
+    each time_values.
+
+    Returns:
+    NDArray[np.float_]: The expected values of the observable. Should be of shape
+    (len(time_values), len(observables)).
+    """
+    observables_expected_values = np.empty(len(time_values), len(observables))
+
+    trotter_circuit = trotter_circuit(hamiltonian, time_values, num_trotter_steps)
+
+
+
+    return observables_expected_values
+
+def trotter_circuit(
+hamiltonian: SparsePauliOp,
+total_duration: Union[float, Parameter],
+num_trotter_steps: int,
+) -> QuantumCircuit:
+    """
+    Construct the QuantumCircuit using the first order Trotter formula.
+
+    Args:
+    hamiltonian (SparsePauliOp): The Hamiltonian which governs the evolution.
+    total_duration (Union[float, Parameter]): The duration of the complete evolution.
+    num_trotter_steps (int): The number of trotter steps.
+
+    Returns:
+    QuantumCircuit: The circuit of the Trotter evolution operator
+    """
+
+    qc = QuantumCircuit(1)
+
+    return qc
+
+
+
+def random_pauli_op(dimension):
+    """
+    Generate a random SparsePauliOp of dimension `dimension`.
+    """
+    pauli_labels = ['I', 'X', 'Y', 'Z']
+    pauli_matrix = np.random.choice(pauli_labels, size=dimension)
+    return SparsePauliOp.from_label(''.join(pauli_matrix))
+
+def create_hamiltonian(num_terms, num_qubits):
+
+    
+    """
+    Generate a random Hamiltonian with `num_terms` terms of dimension `dimension`.
+    """
+    hamiltonian = SparsePauliOp.zeros(num_qubits)  # Start with a zero operator
+
+    for _ in range(num_terms):
+        coefficient = np.random.uniform(-1, 1)  # Random coefficient between -1 and 1
+        pauli_op = random_pauli_op(num_qubits)
+        term = coefficient * pauli_op
+        hamiltonian += term
+
+    return hamiltonian
+
+def create_initial_state(num_qubits: int):
+    qc = QuantumCircuit(num_qubits)
+    return qc
+
+def create_observables(num_qubits: int):
+    ones = np.ones(num_qubits)
+    zeros = np.zeros(num_qubits)
+    pauli_x = Pauli(zeros, ones)
+    pauli_y = Pauli(ones, ones)
+    pauli_z = Pauli(ones, zeros)
+    return PauliList([pauli_x, pauli_y, pauli_z])
+
---
+++ Projet4/main.py
@@ -0,0 +1,80 @@
+##########################################################################
+
+# Titre: main.py
+# Author: Christopher Sicotte (SICC2201)
+# last modified: 26/03/2024
+
+##########################################################################
+'''
+# Description: 
+
+C'est ce fichier qui sera lancé par l'utilisateur pour lancer l'algorithme.
+
+'''
+
+###########################################################################
+
+# IMPORTS
+
+###########################################################################
+import os
+import time
+import numpy as np
+
+# Custom libraries
+import IBMQ_credentials
+import Quantum_evolution as qevo
+import Utils
+
+###########################################################################
+
+# MAIN
+
+###########################################################################
+
+
+def main():
+    start_time = time.time()
+
+    ibmq_token = "put your token here"
+    backend_name = "ibmq_qasm_simulator"
+    directory_path = "BSQ101_projects/Projet3/project_code/h2_mo_integrals"
+    file_paths = [os.path.join(directory_path, file_path) for file_path in os.listdir(directory_path)]
+
+    ####  uncomment if this is the first time you connect to your IBMQ account  ##########
+
+    # IBMQ_credentials.ibmq_connexion(ibmq_token)
+
+    #################################################################################
+
+    #_, backend = IBMQ_credentials.ibmq_provider(backend_name)
+
+    backend = IBMQ_credentials.get_local_simulator()
+    execute_opts = {'shots': 512}
+
+    num_qubits = 4
+    num_terms = 3
+    
+    observables = 0
+    num_trotter_steps = 4
+    time_values = np.arange(0, 10, 0.1)
+    print('time_values: ', time_values)
+
+    hamiltonian = qevo.create_hamiltonian(num_terms, num_qubits)
+    initial_state = qevo.create_initial_state(num_qubits)
+
+    exact_evolution_expected_values = qevo.exact_evolution(initial_state, hamiltonian, time_values, observables)
+    trotter_evolution_expected_values = qevo.trotter_evolution(initial_state, hamiltonian, time_values, observables, num_trotter_steps)
+
+
+    end_time = time.time()
+    print('Runtime: ', end_time-start_time, 'sec')
+
+
+    error = Utils.validate_results(trotter_evolution_expected_values, exact_evolution_expected_values)
+    print('MSE: ', error)
+    return 0
+
+
+if __name__ == "__main__":
+    main()
